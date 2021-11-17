89b35804bfae89a4160020c88b05941535fa4cce
Timofey <timoxa17@gmail.com>
2021-11-17 11:33:40 +0300

    autotests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  root: true,
-  extends: ['airbnb-base'],
   env: {
     browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  plugins: ['prettier', 'jest'],
+  extends: ['eslint:recommended', 'plugin:jest/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   rules: {
+    'no-var': 'error',
+    'prettier/prettier': 'error',
     'prefer-const': 0,
     'no-alert': 0,
     'no-param-reassign': [2, { props: false }],
